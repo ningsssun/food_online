a43c9070a19e31aa4f355796b70c0f58ba47683d
sunning <ning.sssun@gmail.com>
2024-08-15 21:16:23 +0400

    user registration feature

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,7 +8,6 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print('user profile is created')
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
@@ -16,9 +15,7 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
         except:
             # create the user profile is not exist
             UserProfile.objects.create(user=instance)
-            print('profile is not exist, but I created one')
-        print('user is updated')
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, 'this user is being saved')
+    pass

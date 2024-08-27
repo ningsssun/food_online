cf0de1113512e12264c38a033d5254fc19492140
sunning <ning.sssun@gmail.com>
2024-08-27 19:08:20 +0200

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from vendor.forms import VendorForm
 
 
 def vprofile(request):
+    profile_form = UserProfileForm()
+    vendor_form = VendorForm()
     return render(request, 'vendor/vprofile.html')

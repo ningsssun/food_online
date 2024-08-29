d340fd70e378475f87c1a24f8711b406b2f988ed
sunning <ning.sssun@gmail.com>
2024-08-29 17:00:43 +0200

    vendor profile form

--- vendor/forms.py
+++ vendor/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Vendor
+from accounts.validators import allow_only_images_validator
 
 
 class VendorForm(forms.ModelForm):
-    vendor_license = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
--- vendor/views.py
+++ vendor/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
+from accounts.views import check_role_vendor
 from vendor.forms import VendorForm
 from vendor.models import Vendor
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)

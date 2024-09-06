bf965010a123baaf0b5df06c1a97f03600a956bb
sunning <ning.sssun@gmail.com>
2024-09-06 14:58:04 +0200

    vendor profile update

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -158,3 +159,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <developer.tttest@gmail.com>'
+
+
+GOOGLE_API_KEY = 'AIzaSyAnzkt5c7Si0V4P_dPoFbw9FDW5NeWHRkk'
